6b2c63486e11aefedf64bcf19fd75f3154985b36
vamsi <vamsiplaty@gmail.com>
2026-02-23 23:17:26 +0530

    fix : correct double-locking

--- src/SingletonDesignPattern/DoubleCheckMethod.java
+++ src/SingletonDesignPattern/DoubleCheckMethod.java
@@ -2,20 +2,24 @@ package SingletonDesignPattern;
 
 public class DoubleCheckMethod {
 
-    private static DoubleCheckMethod doubleCheckMethod;
     private DoubleCheckMethod(){
         System.out.println("create");
     }
 
-    public static synchronized DoubleCheckMethod getInstance(){
-        if(doubleCheckMethod == null){
-            synchronized(DoubleCheckMethod.class){
-                if(doubleCheckMethod == null){
-                    doubleCheckMethod =  new DoubleCheckMethod();
+
+    // The volatile keyword ensures that multiple threads handle the instance variable correctly when it is being initialized to the Singleton instance.
+    private static volatile DoubleCheckMethod instance;
+
+    // This method uses double-checked locking to reduce the overhead of acquiring a lock by first checking the instance without synchronization.
+    // Only if the instance is null does it synchronize and check again before creating the instance.
+    public static DoubleCheckMethod getInstance() {
+        if (instance == null) {                // First check (no locking)
+            synchronized (DoubleCheckMethod.class) {
+                if (instance == null) {        // Second check (with locking)
+                    instance = new DoubleCheckMethod();
                 }
-                return doubleCheckMethod;
             }
         }
-        return doubleCheckMethod;
+        return instance;
     }
 }
